2563bc613c9c3d5951322abf110b81ecf7c65cb4
guolei <949045897@qq.com>
2019-05-19 15:54:07 +0800

    测试

--- src/main/gl/hello/hello.java
+++ src/main/gl/hello/hello.java
@@ -3,9 +3,15 @@ package gl.hello;
 /**
  * Created by guolei on 2019/5/19.
  */
+
 public class hello {
     public static void main(String[] args)
     {
-        System.out.println("真的傻逼？？？");
+        System.out.println("真的傻逼吗   ？？？");
+        System.out.println("也许吧");
+
+
     }
+
+
 }
